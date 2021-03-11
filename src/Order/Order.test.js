01c24940c686b96f19e05473a598d3d58de81809
yletamitlu <catherinaleksandrovna@gmail.com>
2021-03-11 21:50:52 +0400

    Алексеева Екатерина

--- src/Order/Order.test.js
+++ src/Order/Order.test.js
@@ -1,8 +1,58 @@
+jest.mock('../utils/getDate');
+
+/* eslint-disable import/first */
 import React from 'react'
+import Order from "./Order";
+import {configure, shallow, render} from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import {getDate} from "../utils/getDate";
+
+configure({ adapter: new Adapter() });
 
 describe('Order.js', () => {
-  it('some test', () => {
+    beforeEach(() => {
+        getDate.mockReturnValue('27 11 1998')
+    });
 
-  });
+    it('should render a Order', () => {
+        const order = {shop: 'shop', date: 'date'}
+        const component = shallow(<Order order={order}/>);
+
+        expect(component).toMatchSnapshot();
+    });
+
+    it('Order div counter', () => {
+        const order = {
+            shop: 'shop',
+            date: 'date',
+            "items": Array ['item'],}
+        const component = render(<Order order={order}/>);
+
+        expect(component.find('div')).toHaveLength(2);
+        expect(component).toMatchSnapshot();
+    });
+
+    it('Order with empty date', () => {
+        const order = {shop: 'shop', date: null};
+        const component = shallow(<Order order={order}/>);
+
+        expect(component.find('div')).toHaveLength(0);
+        expect(component).toMatchSnapshot();
+    });
+
+    it('Order with empty order', () => {
+        const order = null;
+        const component = shallow(<Order order={order}/>);
+
+        expect(component.find('div')).toHaveLength(0);
+        expect(component).toMatchSnapshot();
+    });
+
+    it('Order with items', () => {
+        const order = {shop: 'shop', date: 100500, items: ["some data"]}
+        const component = shallow(<Order order={order}/>);
+
+        expect(component.find('div')).toHaveLength(4);
+        expect(component).toMatchSnapshot();
+    });
 });
-
--- src/utils/sortOrders.test.js
+++ src/utils/sortOrders.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {sortByItemCount} from './sortOrders';
+import {sortByDate, sortByItemCount} from './sortOrders';
 
 describe('sortByItemCount function', () => {
 	it('orders are null', () => {
@@ -20,5 +20,245 @@ describe('sortByItemCount function', () => {
 
 		expect(result).toBe(0);
 	});
+
+	it('more than first', () => {
+		const order1 = {
+			items: ['item1', 'item2'],
+		};
+
+		const order2 = {
+			items: ['1', '2', '3'],
+		};
+
+		const result = sortByItemCount(order1, order2);
+
+		expect(result).toBe(-1);
+	});
+
+	it('more than second', () => {
+		const order1 = {
+			items: ['item1', 'item2', 'item3'],
+		};
+
+		const order2 = {
+			items: ['1', '2'],
+		};
+
+		const result = sortByItemCount(order1, order2);
+
+		expect(result).toBe(1);
+	});
+
+	it('orders are undefined', () => {
+		const result = sortByItemCount(undefined, undefined);
+		expect(result).toEqual(0);
+	});
+
+	it('all items are null', () => {
+		const order1 = {
+			items: null,
+		};
+
+		const order2 = {
+			items: null,
+		};
+
+		const result = sortByItemCount(order1, order2);
+
+		expect(result).toBe(0);
+	});
+
+	it('items1 contains null', () => {
+		const order1 = {
+			items: [null],
+		};
+
+		const order2 = {
+			items: ['1'],
+		};
+
+		const result = sortByItemCount(order1, order2);
+
+		expect(result).toBe(0);
+	});
+
+	it('items2 contains null', () => {
+		const order1 = {
+			items: ['1'],
+		};
+
+		const order2 = {
+			items: [null],
+		};
+
+		const result = sortByItemCount(order1, order2);
+
+		expect(result).toBe(0);
+	});
+
+	it('items are undefined', () => {
+		const order1 = {
+			items: undefined,
+		};
+
+		const order2 = {
+			items: undefined,
+		};
+
+		const result = sortByItemCount(order1, order2);
+
+		expect(result).toBe(0);
+	});
+
+	it('items1 contains undefined', () => {
+		const order1 = {
+			items: [undefined],
+		};
+
+		const order2 = {
+			items: ['1'],
+		};
+
+		const result = sortByItemCount(order1, order2);
+
+		expect(result).toBe(0);
+	});
+
+	it('items2 contains undefined', () => {
+		const order1 = {
+			items: ['1'],
+		};
+
+		const order2 = {
+			items: [undefined],
+		};
+
+		const result = sortByItemCount(order1, order2);
+
+		expect(result).toBe(0);
+	});
 });
 
+describe('sortByDate function', () => {
+	it('orders are null', () => {
+		const result = sortByDate(null, null);
+		expect(result).toEqual(0);
+	});
+
+	it('same dates', () => {
+		const order1 = {
+			date1: new Date(1998, 11, 27),
+		};
+
+		const order2 = {
+			date1: new Date(1998, 11, 27),
+		};
+
+		const result = sortByDate(order1, order2);
+
+		expect(result).toBe(0);
+	});
+
+	it('more than first', () => {
+		const order1 = {
+			date: new Date(1998, 1, 27),
+		};
+
+		const order2 = {
+			date: new Date(1998, 11, 14),
+		};
+
+		const result = sortByDate(order1, order2);
+
+		expect(result).toBe(1);
+	});
+
+	it('more than second', () => {
+		const order1 = {
+			date: new Date(1998, 1, 14),
+		};
+
+		const order2 = {
+			date: new Date(1998, 11, 27),
+		};
+
+		const result = sortByDate(order1, order2);
+
+		expect(result).toBe(1);
+	});
+
+	it('orders are undefined', () => {
+		const result = sortByDate(undefined, undefined);
+		expect(result).toEqual(0);
+	});
+
+	it('all dates are null', () => {
+		const order1 = {
+			date1: null,
+		};
+
+		const order2 = {
+			date2: null,
+		};
+
+		const result = sortByDate(order1, order2);
+
+		expect(result).toBe(0);
+	});
+
+	it('date1 is null', () => {
+		const order1 = {
+			date1: null,
+		};
+
+		const order2 = {
+			date2: new Date(1998, 1, 14),
+		};
+
+		const result = sortByDate(order1, order2);
+
+		expect(result).toBe(0);
+	});
+
+	it('date2 is null', () => {
+		const order1 = {
+			date1: new Date(1998, 1, 14),
+		};
+
+		const order2 = {
+			date2: null,
+		};
+
+		const result = sortByDate(order1, order2);
+
+		expect(result).toBe(0);
+	});
+
+	it('date1 is undefined', () => {
+		const order1 = {
+			date1: undefined,
+		};
+
+		const order2 = {
+			date2: new Date(1998, 1, 14),
+		};
+
+		const result = sortByDate(order1, order2);
+
+		expect(result).toBe(0);
+	});
+
+	it('date2 is undefined', () => {
+		const order1 = {
+			date1: new Date(1998, 1, 14),
+		};
+
+		const order2 = {
+			date2: undefined,
+		};
+
+		const result = sortByDate(order1, order2);
+
+		expect(result).toBe(0);
+	});
+});
